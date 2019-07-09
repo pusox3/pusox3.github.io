87062590b7f635e2572f89c75f9f7877ef7dd728
Neil Opena <neil1114o@gmail.com>
2019-07-09 19:15:29 -0400

    Updated dataset

--- static-2019-2020/assets/js/family.js
+++ static-2019-2020/assets/js/family.js
@@ -5,7 +5,14 @@ var nodes = new vis.DataSet([
     { id: 3, label: 'Dope'},
     { id: 4, label: 'Whack'},
     { id: 5, label: 'Bruh'},
-    { id: 6, label: 'Yeet'}
+    { id: 6, label: 'Yeet'},
+    { id: 7, label: 'Yeet'},
+    { id: 8, label: 'Yeet'},
+    { id: 9, label: 'Yeet'},
+    { id: 10, label: 'Yeet'},
+    { id: 11, label: 'Yeet'},
+    { id: 12, label: 'Yeet'},
+    { id: 13, label: 'Yeet'}
 ]);
 
 /* Blue: #2980b9 */
@@ -16,7 +23,16 @@ var edges = new vis.DataSet([
     { from: 1, to: 2, color: { color: '#d63031', highlight: '#d63031' } },
     { from: 2, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } },
     { from: 2, to: 5, color: { color: '#d63031', highlight: '#d63031' } },
-    { from: 6, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } }
+    { from: 6, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 4, to: 7, color: { color: '#d63031', highlight: '#d63031' } },
+    { from: 5, to: 7, color: { color: '#d63031', highlight: '#d63031' } },
+    { from: 8, to: 5, color: { color: '#d63031', highlight: '#d63031' } },
+    { from: 9, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 9, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 10, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 11, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 12, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 13, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
 ]);
 
 // create a network
@@ -78,5 +94,13 @@ var options = {
     }
 }
 
-// initialize your network!
 var network = new vis.Network(container, data, options);
+
+// const network = new vis.Network(container, data, options);
+// setTimeout(()=>{
+//     network.setOptions({
+//         layout:{
+//             hierarchical: false
+//         },
+//     });
+// },1000);
