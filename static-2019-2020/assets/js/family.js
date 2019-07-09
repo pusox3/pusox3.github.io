1d629a357715df2dd3e53c73db90b46611c3c196
Neil Opena <neil1114o@gmail.com>
2019-07-09 18:52:28 -0400

    Modified colors of the network

--- static-2019-2020/assets/js/family.js
+++ static-2019-2020/assets/js/family.js
@@ -7,12 +7,15 @@ var nodes = new vis.DataSet([
     { id: 5, label: 'Node 5' }
 ]);
 
+/* Blue: #2980b9 */
+/* Red: #d63031 */
+
 // create an array with edges
 var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 }
+    { from: 1, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 1, to: 2, color: { color: '#d63031', highlight: '#d63031' } },
+    { from: 2, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } },
+    { from: 2, to: 5, color: { color: '#d63031', highlight: '#d63031' } }
 ]);
 
 // create a network
@@ -28,27 +31,51 @@ var options = {
     height: '100%',
     width: '100%',
     locale: 'en',
+    nodes: {
+        borderWidth: 2,
+        borderWidthSelected: 3,
+        color: {
+            border: '#fdcb6e',
+            background: '#ffeaa7',
+            highlight: {
+                border: '#fdcb6e',
+                background: '#fdcb6e'
+            }
+        }
+    },
     edges: {
+        width: 3,
         arrows: {
-            to: { enabled: true, scaleFactor: 1, type: 'arrow' }
+            to: { enabled: true, type: 'arrow' }
         },
-        color: 'red',
-        font: '12px arial #ff0000',
         scaling: {
             label: true,
         },
         shadow: true,
-        smooth: true,
+        smooth: {
+            enabled: true,
+            type: 'dynamic',
+            roundness: 0.5
+        }
     },
-    // clickToUse: false,
-    // configure: { ...},    // defined in the configure module.
-    // edges: { ...},        // defined in the edges module.
-    // nodes: { ...},        // defined in the nodes module.
-    // groups: { ...},       // defined in the groups module.
-    // layout: { ...},       // defined in the layout module.
-    // interaction: { ...},  // defined in the interaction module.
-    // manipulation: { ...}, // defined in the manipulation module.
-    // physics: { ...},      // defined in the physics module.
+    physics: {
+        enabled: true,
+        timestep: 0.2,
+        maxVelocity: 20,
+        minVelocity: 0.2,
+        hierarchicalRepulsion: {
+            centralGravity: 0.0
+        },
+        solver: 'hierarchicalRepulsion'
+    },
+    layout: {
+        randomSeed: undefined,
+        hierarchical: {
+            enabled: true,
+            direction: 'DU',
+            sortMethod: 'directed'
+        }
+    }
 }
 
 // initialize your network!
