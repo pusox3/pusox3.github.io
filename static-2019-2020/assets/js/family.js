18a3790fe06fb35c758e50a5333164a86ffd6303
Neil Opena <neil1114o@gmail.com>
2019-07-09 18:57:26 -0400

    Modified dataset

--- static-2019-2020/assets/js/family.js
+++ static-2019-2020/assets/js/family.js
@@ -1,10 +1,11 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-    { id: 1, label: 'Node 1' },
-    { id: 2, label: 'Node 2' },
-    { id: 3, label: 'Node 3' },
-    { id: 4, label: 'Node 4' },
-    { id: 5, label: 'Node 5' }
+    { id: 1, label: 'LMAO'},
+    { id: 2, label: 'XD'},
+    { id: 3, label: 'Dope'},
+    { id: 4, label: 'Whack'},
+    { id: 5, label: 'Bruh'},
+    { id: 6, label: 'Yeet'}
 ]);
 
 /* Blue: #2980b9 */
@@ -15,7 +16,8 @@ var edges = new vis.DataSet([
     { from: 1, to: 3, color: { color: '#2980b9', highlight: '#2980b9' } },
     { from: 1, to: 2, color: { color: '#d63031', highlight: '#d63031' } },
     { from: 2, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } },
-    { from: 2, to: 5, color: { color: '#d63031', highlight: '#d63031' } }
+    { from: 2, to: 5, color: { color: '#d63031', highlight: '#d63031' } },
+    { from: 6, to: 4, color: { color: '#2980b9', highlight: '#2980b9' } }
 ]);
 
 // create a network
@@ -69,7 +71,6 @@ var options = {
         solver: 'hierarchicalRepulsion'
     },
     layout: {
-        randomSeed: undefined,
         hierarchical: {
             enabled: true,
             direction: 'DU',
